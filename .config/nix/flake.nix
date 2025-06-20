499c661b6b0b89e0c020412564e38ca03b24dfe1
Selene Blok <selene.blok@gmail.com>
2025-06-21 00:02:39 +0200

    feat(nix): Update nix configuration

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -13,14 +13,22 @@
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [ pkgs.vim
+        [ 
+          pkgs.neovim,
+          pkgs.tmux,
+          pkgs.git,
         ];
+
+      fonts.packages = [
+        (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ] })
+      ]
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
       # Enable alternative shell support in nix-darwin.
       # programs.fish.enable = true;
+      programs.zsh.enable = true;
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
@@ -28,6 +36,8 @@
       # Used for backwards compatibility, please read the changelog before changing.
       # $ darwin-rebuild changelog
       system.stateVersion = 6;
+
+      system.primaryUser = "seleneblok";
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
