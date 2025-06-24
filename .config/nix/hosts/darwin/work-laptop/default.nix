a8a691bf25efe3bd3f0211f6db5c459c289fd0dc
Selene Blok <selene.blok@gmail.com>
2025-06-24 09:36:44 +0200

    fix(nix): Update work laptop config

--- .config/nix/hosts/darwin/work-laptop/default.nix
+++ .config/nix/hosts/darwin/work-laptop/default.nix
@@ -2,10 +2,11 @@
 {
   imports = [
     ../shared/configuration.nix
+    ./software.nix
   ];
   nixpkgs.hostPlatform = "x86_64-darwin";
   system.primaryUser = "seleneblok";
-  system.security.pam.services.sudo_local.touchIdAuth = true;
+  security.pam.services.sudo_local.touchIdAuth = true;
   networking.knownNetworkServices = [
     "Thunderbolt Bridge"
     "Wi-Fi"
---
+++ .config/nix/hosts/darwin/work-laptop/software.nix
@@ -0,0 +1,19 @@
+{ pkgs, lib, ... }: {
+  imports = [
+    ../shared/software.nix
+  ];
+
+  environment.systemPackages = lib.mkAfter (with pkgs; []);
+
+  # `brew list <>` can help pinpoint package name
+  # for both ordinary packages and casks
+  homebrew.brews = lib.mkAfter [];
+
+  homebrew.casks = lib.mkAfter [
+    # Shared apps
+    "displaylink"
+  ];
+
+  # `mas search <>` can help pinpoint package name
+  homebrew.masApps = lib.mkAfter {};
+}
